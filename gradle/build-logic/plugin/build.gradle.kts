0e133d5b5f17c494903a3c804535556972af32a7
Willpasco <will.pasco.az@gmail.com>
2025-03-21 20:56:31 -0300

    Feat: Creating the build logic module.

---
+++ gradle/build-logic/plugin/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.will.meli.plugin"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+dependencies {
+    compileOnly(libs.android.gradle.plugin)
+    compileOnly(libs.kotlin.gradle.plugin)
+}
+
+gradlePlugin {
+    plugins {
+        register("conventionPluginAndroidApplication"){
+            id = "meli.android.application"
+            implementationClass = "com.will.meli.plugin.AndroidApplicationConventionPlugin"
+        }
+        register("conventionPluginCompose"){
+            id = "meli.compose"
+            implementationClass = "com.will.meli.plugin.ComposeConventionPlugin"
+        }
+    }
+}
