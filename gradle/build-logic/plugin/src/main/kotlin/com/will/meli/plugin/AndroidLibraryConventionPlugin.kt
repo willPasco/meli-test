f1777e405758cf18b09fbc1fc6bd19bfbe0d94ed
Willpasco <will.pasco.az@gmail.com>
2025-03-21 22:01:05 -0300

    Creating the network module.

--- gradle/build-logic/plugin/src/main/kotlin/com/will/meli/plugin/AndroidLibraryConventionPlugin.kt
+++ gradle/build-logic/plugin/src/main/kotlin/com/will/meli/plugin/AndroidLibraryConventionPlugin.kt
@@ -1,6 +1,7 @@
 package com.will.meli.plugin
 
 import com.android.build.api.dsl.ApplicationExtension
+import com.android.build.api.dsl.LibraryExtension
 import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -9,21 +10,27 @@ import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.getByType
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-class AndroidApplicationConventionPlugin : Plugin<Project> {
+class AndroidLibraryConventionPlugin : Plugin<Project> {
 
     override fun apply(target: Project) {
         with(target) {
             val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
 
             with(pluginManager) {
-                apply(libs.findPlugin("android.application").get().get().pluginId)
+                apply(libs.findPlugin("android.library").get().get().pluginId)
                 apply(libs.findPlugin("kotlin.android").get().get().pluginId)
             }
 
-            extensions.configure<ApplicationExtension> {
+            extensions.configure<LibraryExtension> {
                 baseConfig(this, libs)
+                defaultConfig.consumerProguardFiles("consumer-rules.pro")
+            }
+
+            extensions.configure<KotlinAndroidProjectExtension>(){
+                explicitApi()
             }
         }
     }
