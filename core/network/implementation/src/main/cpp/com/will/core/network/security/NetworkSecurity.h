3fd4b1e47189e9e734ca093112baa4b7883cee80
Willpasco <will.pasco.az@gmail.com>
2025-03-22 13:12:10 -0300

    Deleting unnused lib.

--- core/network/implementation/src/main/cpp/com/will/core/network/security/NetworkSecurity.h
+++ core/network/implementation/src/main/cpp/com/will/core/network/security/NetworkSecurity.h
@@ -3,5 +3,7 @@
 #include <jni.h>
 
 extern "C"
-jstring Java_com_will_core_network_implementation_security_NetworkSecurityImpl_getAccessToken(JNIEnv *env,
-                                                                                              __attribute__((unused)) jobject object);
+jstring Java_com_will_core_network_implementation_security_NetworkSecurityImpl_getAccessToken(
+        JNIEnv *env,
+        __attribute__((unused)) jobject object
+);
