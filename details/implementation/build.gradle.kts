3220f83589c9927c9836ccf7104e63cc95c0bc72
William Pasco de Azevedo <36263411+willPasco@users.noreply.github.com>
2025-03-22 18:05:42 -0300

    Feat: Creating the details module. (#13)
    ### Checklist
    
    - [ ] A compilação foi bem sucedida?
    - [ ] Contém testes unitários?
    - [ ] Contém testes de ui?
    
    
    ### Descrição do PR
    Criando o módulo que será responsável por exibir os detalhes do produto.
    
    ### Evidência
    N/A

---
+++ details/implementation/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    alias(libs.plugins.convention.android.library)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.convention.compose)
+}
+
+android {
+    namespace = "com.will.details.implementation"
+}
+
+dependencies {
+    implementation(project(":details:api"))
+    implementation(project(":core:navigation:api"))
+    implementation(project(":core:network:api"))
+    implementation(project(":core:style"))
+
+    implementation(libs.koin.android)
+    implementation(libs.koin.androidx.compose)
+    implementation(libs.androidx.material3)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
+
+    testImplementation(libs.bundles.unit.test)
+}
