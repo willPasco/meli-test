24637176514e34249f1292011987c31798669478
William Pasco de Azevedo <36263411+willPasco@users.noreply.github.com>
2025-03-24 23:59:21 -0300

    Feat: Creating the listing screen instrumented test. (#28)
    ### Checklist
    
    - [x] A compilação foi bem sucedida?
    - [ ] Contém testes unitários?
    - [x] Contém testes de ui?
    
    
    ### Descrição do PR
    Criando os testes instrumentados da listing screen.
    
    ### Evidência
    N/A

--- listing/implementation/src/main/kotlin/com/will/listing/implementation/presentation/composable/component/SearchSection.kt
+++ listing/implementation/src/main/kotlin/com/will/listing/implementation/presentation/composable/component/SearchSection.kt
@@ -20,6 +20,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
@@ -60,6 +61,7 @@ internal fun SearchSection(
                 drawRect(animatedColor)
             }
             .padding(top = animatedPadding)
+            .testTag("search")
     ) {
 
         var text by rememberSaveable { mutableStateOf("") }
@@ -67,7 +69,8 @@ internal fun SearchSection(
         OutlinedTextField(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp),
+                .padding(16.dp)
+                .testTag("search-input"),
             value = text,
             shape = RoundedCornerShape(40.dp),
             colors = TextFieldDefaults.colors(
