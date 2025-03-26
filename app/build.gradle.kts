cb931b14f8def199a5df1f988f3bba3a1243f924
Willpasco <will.pasco.az@gmail.com>
2025-03-25 22:42:10 -0300

    Feat: Creating the release workflow.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.will.meli"
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1"
     }
 
     buildTypes {
